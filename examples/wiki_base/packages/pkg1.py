df57a7fae267faad32669d889c285a1b881135f3
Simone Campagna <simone.campagna@tiscali.it>
2013-12-07 14:49:30 +0100

    Added short and long description

--- examples/wiki_base/packages/pkg1.py
+++ examples/wiki_base/packages/pkg1.py
@@ -1,6 +1,10 @@
 from zapper.package_file import *
 
 pkg1 = Product('pkg1', 'application')
+pkg1.set_short_description("pkg1 is an example")
+pkg1.set_long_description("""\
+This is an example of a long description for the pkg1 package. The description text will be inherited by all the packages.
+A package can override it.""")
 
 pkg1_4_2 = Package(pkg1, '4.2')
 pkg1_4_2.var_set("PKG1_HOME", "/opt/install/pkg1-4.2")
